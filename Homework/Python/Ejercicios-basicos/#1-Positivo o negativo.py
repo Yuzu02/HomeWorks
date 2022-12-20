bba3055ad99a552527e6be336d28a30fb01c0b3d
Daell Lied <57969791+Yuzu02@users.noreply.github.com>
2022-12-19 22:24:32 -0400

    #1
    Pequeño codigo en python para determinar si un numero es negativo o positivo

---
+++ Homework/Python/Ejercicios-basicos/#1-Positivo o negativo.py
@@ -0,0 +1,27 @@
+#Toma un número entero y determina si es positivo o negativo
+numero = int(input("Ingrese un número entero: ")) 
+
+#Si el número es mayor o igual a 0, es positivo
+if numero >= 0:
+  #Imprime el numero es Positivo.
+  print("El número es positivo")
+
+#Si el número es menor a 0, es negativo
+elif numero < 0:
+  #Imprime el numero es negativo
+  print("El número es negativo")
+
+else: numero == 0
+#imprimee el numero es 0
+print("El número es 0, por lo tanto es nulo")
+
+  #Ejemplo de salida
+  
+  #Ingrese un número entero: 5
+  #El número es positivo
+
+  #Ingrese un número entero: -5
+  #El número es negativo
+  
+  #Ingrese un número entero: 0
+  #El número es el número es 0, por lo tanto es nulo
