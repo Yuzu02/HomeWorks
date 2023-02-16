fbb0a9b6e785305b8888ae5dd5e889c354e5afcb
Auri06 <118132700+Auri06@users.noreply.github.com>
2023-02-16 12:39:17 -0400

    Add files via upload
    es un programa hecho ne python para hacer una presentacion con imput

---
+++ prensentacion.py
@@ -0,0 +1,13 @@
+print("estas mi presentacion personal")
+nombre=input("ingresa tu nombre")
+apellido=input("ingresa tu apellido")
+edad=input("introduce tu edad")
+pais=input("ingresa tu pais")
+ciudad=input("ingresa tu ciudad")
+
+print(" mi nombre es " + nombre+ " y mi apellido es "+ apellido + " tengo "+ str(edad) + "años de edad" + " vivo en " + pais + " en la ciudad de " + ciudad)
+
+
+
+
+
