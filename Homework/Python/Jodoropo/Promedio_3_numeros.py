cc9e4d35139cfbe412cebcd984b48f9e48c4783b
Yuzu02 <57969791+Yuzu02@users.noreply.github.com>
2023-03-01 00:48:55 -0400

    #2
    Algoritmo en Python para hacer el promedio de 3 numeros cualquiera y mostrarlo en terminal.

---
+++ Homework/Python/Jodoropo/Promedio_3_numeros.py
@@ -0,0 +1,20 @@
+# Promedio de 3 numeros
+
+# Ingreso de datos
+
+num1 = int(input("Ingrese el primer numero: "))
+
+# Tomamos el segundo numero
+
+num2 = int(input("Ingrese el segundo numero: "))
+
+# Tomamos el ultimo numero
+
+num3 = int(input("Ingrese el tercer numero: "))
+
+# Hacemos la operacion
+
+resultado = float (num1 + num2 + num3) / 3
+
+# imprimimos el resultado
+print(resultado)
