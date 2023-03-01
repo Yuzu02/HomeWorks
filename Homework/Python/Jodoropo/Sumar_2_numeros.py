104010c0d1258c743ee6e846ad7f02db54f91b3d
Yuzu02 <57969791+Yuzu02@users.noreply.github.com>
2023-03-01 00:45:59 -0400

    #1
    Pequeño algoritmo en python para sumar 2 numeros cualesquieras.

---
+++ Homework/Python/Jodoropo/Sumar_2_numeros.py
@@ -0,0 +1,15 @@
+# Sumatoria de 2 numeros
+
+# Ingreso de datos
+num1 = int(input("Ingrese el primer numero: "))
+
+# Tomamos el otro dato
+num2 = int(input("Ingrese el primer numero: "))
+
+# Hacemos la operacion
+
+resultado = (num1 + num2)
+
+# Sumamos los 2 numeros
+
+print(resultado)
