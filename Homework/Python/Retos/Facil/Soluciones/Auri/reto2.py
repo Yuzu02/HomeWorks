4d17ce2c72a9689a921b7f26ac9af29845fb243e
Auri06 <118132700+Auri06@users.noreply.github.com>
2023-03-04 21:16:29 -0400

    Add files via upload

---
+++ Homework/Python/Retos/Facil/Soluciones/Auri/reto2.py
@@ -0,0 +1,8 @@
+#Suma de dos números: Escribe un programa que 
+#pida al usuario dos números y luego muestre la suma de esos dos números
+numero1=(int(input("introduce un numero ")))
+#int se utiliza para especificar un numero
+#input se utiliza para pedir informacion en consola
+numero2=(int(input("introduce un numero")))
+resultado=numero1+numero2
+print(resultado)
